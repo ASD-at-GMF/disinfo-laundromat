9f294e0735118a7b9e9eb359ec3b9def57f2dc44
Peter Benzoni <benzonip@gmail.com>
2023-01-10 17:16:04 -0500

    refactoring

--- crawler.py
+++ crawler.py
@@ -209,7 +209,7 @@ for item in ignore_sites:
     item.lower()
 
 visited = set()
-verification_tuples = []
+attribution_tuples = []
 
 
 def valid_url(url):
@@ -241,7 +241,7 @@ def get_domain_name(url):
     return domain_name
 
 
-def get_ip_address(domain_name):
+def add_ip_address(domain_name):
 
     if domain_name.startswith('https://'):
         domain_name = domain_name[8:]
@@ -249,10 +249,47 @@ def get_ip_address(domain_name):
     try:
         # Resolve the domain name to an IP address
         ip_address = socket.gethostbyname(domain_name)
+        attribution_tuples.append(('ip', ip_address, get_domain_name(domain_name)))
+        last_period_index = ip_address.rfind('.')
+        subnet_id = ip_address[:last_period_index]
+        attribution_tuples.append(('subnet', subnet_id, get_domain_name(domain_name)))
+
         print("The IP address of the domain name {} is {}".format(
             domain_name, ip_address))
     except socket.gaierror:
         print("Could not resolve the domain name {}".format(domain_name))
+
+
+def get_who_is(url):
+    return whois.whois(url)
+
+
+def add_who_is(url, whois):
+    if 'Private' not in whois.name:
+        attribution_tuples.append(('whois', whois, get_domain_name(url)))
+
+
+def parse_meta_tags(url, soup):
+
+    meta_tags = soup.find_all('meta')
+
+    # Iterate over the meta tags
+    for meta_tag in meta_tags:
+        # Get the name and content attributes of the meta tags
+        name = meta_tag.get('name')
+        content = meta_tag.get('content')
+        if name and 'verification' in name:
+            add_verification_tags(url, name, content)
+        else:
+            print(meta_tag)
+
+
+def add_verification_tags(url, name, content):
+
+    # Print the name and content attributes
+
+    attribution_tuples.append(
+        ('verification-id', name + '|' + content, get_domain_name(url)))
 
 
 def crawl(url, visited_urls):
@@ -266,22 +303,11 @@ def crawl(url, visited_urls):
 
     # Print the DOM
     print(url)
-    get_ip_address(url)
-    w = whois.whois(url)
-    verification_tuples.append(('whois', w, get_domain_name(url)))
-    print(w)
+    add_ip_address(url)
+    whoisResults = get_who_is(url)
+    add_who_is(url, whoisResults)
+    parse_meta_tags(url, soup)
 
-    meta_tags = soup.find_all('meta')
-
-    # Iterate over the meta tags
-    for meta_tag in meta_tags:
-        # Get the name and content attributes of the meta tag
-        name = meta_tag.get('name')
-        content = meta_tag.get('content')
-        # Print the name and content attributes
-        if name and 'verification' in name:
-            print(f'name: {name}, content: {content}')
-        t = ('verification-id', name, content, get_domain_name(url))
     with open('soup.html', 'w', encoding='utf-8', errors='ignore') as file:
         # Write the prettified HTML content to the file
         file.write(soup.prettify())
@@ -297,15 +323,15 @@ def crawl(url, visited_urls):
             href = valid_url(href)
             # If the URL has not been visited yet
             if get_domain_name(href) not in visited_urls and href != "":
-
                 # Follow the link
-                time.sleep(1)
+                #time.sleep(1)
                 try:
-                    #crawl(href, visited_urls)
-                    print()
+                    print(href)
                 except Exception:
                     continue
 
+
 visited_urls = set()
 # Start the crawler at a specific URL
-crawl('https://www.oklahomastar.com', visited_urls)
+crawl('https://www.rt.com', visited_urls)
+print(attribution_tuples)

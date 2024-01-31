86d39931b9e6d822673eafc1567cda2dc5ba0ce0
m <matus.solcany@protonmail.com>
2024-01-31 12:01:06 +0100

    use case list

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -7,8 +7,8 @@
 </script>
 
 <main class="w-100 ">
-	<div class="grid grid-rows-2 gap-4">
-		<section name="search" class="grid grid-cols-1 md:grid-cols-2 gap-4">
+	<section class="grid grid-rows-2 gap-4">
+		<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
 			<div class="bg-blue-300">random</div>
 			<div class="h-[300px]">
 				<TabsRoot value="content similarity">
@@ -20,12 +20,14 @@
 					<TabsContent value="metadata similarity">test 2 test 2</TabsContent>
 				</TabsRoot>
 			</div>
-		</section>
-		<section name="use cases" class="grid grid-cols-1 md:grid-cols-3 gap-x-4">
-			<div class="md:col-span-3 bg-green-300"> Label! </div>
-			<div class="bg-pink-500">test1</div>
-			<div class="bg-pink-500">test2</div>
-			<div class="bg-pink-500">test3</div>
-		</section>
-	</div>
+		</div>
+		<div>
+			<label for="use case list" class="bg-green-300"> Label! </label>
+			<ul id="use case list" class="grid grid-cols-1 md:grid-cols-3 gap-x-4">
+				<li class="bg-pink-500">test1</li>
+				<li class="bg-pink-500">test2</li>
+				<li class="bg-pink-500">test3</li>
+			</ul>
+		</div>
+	</section>
 </main>

832c4b15bd460a471c21c025f7bf617682c4f2b5
Alicia Bargar <aliciabargar@gmail.com>
2024-05-15 21:08:13 +0200

    ensure config variable is passed in

--- init_app.py
+++ init_app.py
@@ -3,7 +3,7 @@ import sqlite3
 from flask import Flask, g
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-# from flask_migrate import Migrate
+from flask_migrate import Migrate
 
 from config import config
 
@@ -11,18 +11,18 @@ APP_SECRET_KEY = os.getenv('APP_SECRET_KEY', '')
 SQLLITE_DB_PATH = os.getenv('SQLLITE_DB_PATH', '')
 
 db = SQLAlchemy()
-# migrate = Migrate()
+migrate = Migrate()
 
 def init_app(config_mode):
     app = Flask(__name__)
     CORS(app, resources={r"/*": {"origins": "*"}})
     app.secret_key = APP_SECRET_KEY  # Set a secret key for security purposes
-    app.config.from_object(config["development"])
+    app.config.from_object(config[config_mode])
     app.config['SESSION_COOKIE_SECURE'] = True
     app.config['SESSION_COOKIE_HTTPONLY'] = True
     app.config['REMEMBER_COOKIE_SECURE'] = True
     db.init_app(app)
-    # migrate.init_app(app, db)
+    migrate.init_app(app, db)
 
     return app
 
